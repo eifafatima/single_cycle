19230371ff56634f91b239036f663a5a760d3793
eifafatima <eifafatima25@gmail.com>
2025-03-15 22:02:20 +0500

    Update Datapath.sv

--- pipeline_3stage/Datapath.sv
+++ pipeline_3stage/Datapath.sv
@@ -1,140 +1,57 @@
 module Datapath(
     input logic clk, reset
 );
-// inc
-    logic [31:0] Pc, ALU_out, extended_immediate, instruction, rdata1, rdata2, ALU_result, readdata, writedata;
-    logic [31:0] PC_f, PC_e, PC_w, PCplus4, sign_extended;
+    logic [31:0] Pc_f, Pc_e, Pc_w, ALU_out, extended_immediate;
     logic [31:0] instruction_f, instruction_f_DFF, instruction_e, instruction_w, ALU_result_e, ALU_result_w;
-    logic [31:0] rdata1_reg, rdata2_reg, rdata1, rdata2, rdata, wdata, wdata_dm;
-    logic [31:0] sel_input_A, sel_input_B;
-
-// c
+    logic [31:0] rdata1_register, rdata2_register, rdata1, rdata2, readdata, writedata, writedata_dm;
     logic [4:0] rsource1, rsource2, rdestination;
     logic [6:0] opcode;
     logic [3:0] alu_operation;
     logic [2:0] branch_type;
-    logic [1:0] wb_select, writeback_sel_out;
+    logic [1:0] writeback_sel_w, writeback_sel;
     logic reg_write, read_enable, write_enable, branch_taken, select_A, select_B;
-// inc
+    logic reg_write_w, read_enable_w, write_enable_w, xo1, xo2, Flush;
 
-    logic register_write_w, read_enable_out, write_enable_out, xo_1, xo_2, Flush;
+    
+    assign opcode = instruction_e[6:0];
+    assign rsource1 = instruction_e[19:15];
+    assign rsource2 = instruction_e[24:20];
+    assign rdestination = instruction_w[11:7];
+    assign ALU_out = branch_taken ? ALU_result_e : Pc_f + 4;
+    assign Flush = branch_taken ? 1'b1 : 1'b0; 
+
+    assign rdata1 = xo1 ? writedata : rdata1_register;
+    assign rdata2 = xo2 ? writedata : rdata2_register;
+        
+    assign instruction_f = Flush ? 32'h00000013 : instruction_f_DFF;
 
     always_comb begin
 
-        opcode = instruction_e[6:0];
-        raddr1 = instruction_e[19:15];
-        raddr2 = instruction_e[24:20];
-        waddr  = instruction_w[11:7];
-        Flush = br_taken ? 1'b1 : 1'b0; 
-
-        rdata1 = xo_1 ? wdata : rdata1_reg;
-        rdata2 = xo_2 ? wdata : rdata2_reg;
-
-        sel_input_A = sel_A ? rdata1 : PC_e;
-        sel_input_B = sel_B ? sign_extended : rdata2;
-
-        instruction_f = Flush ? 32'h00000013 : instruction_f_DFF;
-    end
-// c
-    // Instantiation
-    Pc pc_0 (
-        clk,
-        reset,
-        branch_taken ? ALU_result : ALU_out,
-        Pc
-        );
-
-    Instruction_Memory inst_mem_0 (
-        Pc,
-        instruction
-        );
-
-    Register_File reg_file_0 (
-        clk, 
-        reg_write, 
-        rsource1, 
-        rsource2, 
-        rdestination, 
-        writedata, 
-        rdata1, 
-        rdata2
-        );
+        case (writeback_sel_w)
+            2'b00: writedata = Pc_w + 4;
+            2'b01: writedata = ALU_result_w;
+            2'b10: writedata = readdata;
+        endcase
         
-    ALU alu_0 (
-        alu_operation,
-        select_A ? rdata1 : Pc, 
-        select_B ? extended_immediate : rdata2, 
-        ALU_result
-        );
+    end
 
-    Data_Memory data_mem_0 (
-        clk, 
-        write_enable, 
-        read_enable, 
-        ALU_result, 
-        rdata2, 
-        readdata
-        );
+    // Instantiation
+    DFF dff_00 (clk, reset, Pc_f, Pc_e);
+    DFF dff_01 (clk, reset, instruction_f, instruction_e);
+    DFF dff_02 (clk, reset, Pc_e, Pc_w);
+    DFF dff_03 (clk, reset, ALU_result_e, ALU_result_w);
+    DFF dff_04 (clk, reset, rdata2, writedata_dm);
+    DFF dff_05 (clk, reset, instruction_e, instruction_w);
 
-    Branch_Condition branch_cond_0 (
-        branch_type, 
-        rdata1, 
-        rdata2, 
-        branch_taken
-        );
-
-    Controller controller_0 (
-        instruction, 
-        reg_write, 
-        read_enable, 
-        write_enable,   
-        select_A, 
-        select_B, 
-        wb_select, 
-        alu_operation, 
-        branch_type
-        );
-
-    Immediate_Generator imd_generator_0 (
-        instruction, 
-        extended_immediate
-        );
-    
-    FWD_UNIT fwd_unit_0 (
-         reg_wr_w,
-         inst_e, 
-         inst_w, 
-         xo_1, 
-         xo_2
-         );
-
-    WB_Mux write_back_mux_0 (
-        writeback_sel_w, 
-        ALU_res_w, 
-        rdata, 
-        pc_w, 
-        wdata
-        );
-
-    ControlUnit controlunit_0 (
-        clk, 
-        rst, 
-        register_write_in, 
-        write_enable_in, 
-        read_enable_in, 
-        writeback_sel_in, 
-        register_write_out, 
-        write_enable_out, 
-        read_enable_out, 
-        writeback_sel_out
-        );
-// inc
-    DFF dff_0 (clk, rst, PC_f, PC_e);
-    DFF dff_1 (clk, rst, instruction_f, instruction_e);
-    DFF dff_2 (clk, rst, PC_e, PC_w);
-    DFF dff_3 (clk, rst, ALU_result_e, ALU_result_w);
-    DFF dff_4 (clk, rst, rdata2, wdata_dm);
-    DFF dff_5 (clk, rst, instruction_e, instruction_w);
-
+    Pc pc_0 (clk, reset, ALU_out, Pc_f);
+    Instruction_Memory inst_mem_0 (Pc_f, instruction_f_DFF);
+    Register_File reg_file_0 (clk, reg_write_w, rsource1, rsource2, rdestination, writedata, rdata1_register, rdata2_register);
+    ALU alu_0 (alu_operation, select_A ? rdata1 : Pc_e, select_B ? extended_immediate : rdata2, ALU_result_e);
+    Data_Memory data_mem_0 (clk, write_enable_w, read_enable_w, ALU_result_w, writedata_dm, readdata);
+    Branch_Condition branch_condition_0 (branch_type, rdata1, rdata2, branch_taken);
+    FWD_UNIT forwarding_unit_0 ( reg_write_w, instruction_e, instruction_w, xo1, xo2);
+    Immediate_Generator immediate_generator_0 (instruction_e, extended_immediate);
+    Controller controller_0 (instruction_e, reg_write, read_enable, write_enable, select_A, select_B, writeback_sel, alu_operation, branch_type);
+    ControlUnit control_unit_0 (clk, reset, reg_write, write_enable, read_enable, writeback_sel, reg_write_w, write_enable_w, read_enable_w, writeback_sel_w);
 
 endmodule
